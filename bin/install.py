bc6cabf82599c97a3f35741c51ac7a67b5f690bc
Steven Arcangeli <stevearc@stevearc.com>
2022-11-23 08:04:26 -0800

    fix: formatting

--- bin/install.py
+++ bin/install.py
@@ -58,25 +58,25 @@ def build_exe(ref: str, venv_dir: str, workdir: str, dest: str) -> None:
             package_dir = tar.next()
             if package_dir is None:
                 raise Exception("Downloaded github archive is malformed")
+
             def is_within_directory(directory, target):
-                
+
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-            
+
                 prefix = os.path.commonprefix([abs_directory, abs_target])
-                
+
                 return prefix == abs_directory
-            
+
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-            
+
                 for member in tar.getmembers():
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
-            
-                tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
-            
+
+                tar.extractall(path, members, numeric_owner=numeric_owner)
+
             safe_extract(tar)
     bluepill = os.path.join(workdir, package_dir.name)
 
